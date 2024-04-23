0fec92546035b36b3a6072f9bad31a91e6ca6f9a
KritikaBindal07 <kirtibindalkb770@gmail.com>
2024-04-23 20:40:08 +0530

    forgot passwprd

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -943,22 +943,22 @@ const updatePassword = asyncHandler(async (req, res) => {
 
 const forgotPasswordToken = asyncHandler(async (req, res) => {
   const { email } = req.body;
-  // const user = await Userdb.findOne({ email: email });
-  // console.log(user);
-  // if (!user) {
-  //   throw new Error("User not found");
-  // }
+  const user = await Userdb.findOne({ email: email });
+  console.log(user);
+  if (!user) {
+    throw new Error("User not found");
+  }
   try {
-    const token = "abcd";
-    // await user.save();
-    const resetURL = `Hi, Follow this link to reset your Password. This link will be valid for 10 minutes from now. <a href='http://localhost:8080/api/user/reset-password/${token}'>Click Here</>`;
+    const token = await user.createPasswordResetToken();
+    await user.save();
+    const resetURL = `Hi, Follow this link to reset your Password. This link will be valid for 10 minutes from now. <a href='http://localhost:3000/reset-password/${token}'>Click Here</>`;
     const data = {
       to: email,
       text: "Hey!!",
       subject: "Reset Password",
       htm: resetURL,
     };
-    sendEmail(data);
+    await sendEmail(data);
     res.json(token);
   } catch (error) {
     throw new Error(error);
