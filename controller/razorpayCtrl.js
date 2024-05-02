3c53f84a2e6d3b94a2fea511613ac0ef06d0d617
aditya123473892 <adityathakur6199@gmail.com>
2024-05-03 00:31:36 +0530

    main

--- controller/razorpayCtrl.js
+++ controller/razorpayCtrl.js
@@ -37,7 +37,6 @@ const paymentVerification = asyncHandler(async (req, res) => {
 
   if (isAuthentic) {
     try {
-     
       const updatedOrder = await Orderdb.findOneAndUpdate(
         { _id: orderId },
         {
@@ -45,30 +44,33 @@ const paymentVerification = asyncHandler(async (req, res) => {
             "paymentInfo.razorpay_order_id": razorpay_order_id,
             "paymentInfo.razorpay_payment_id": razorpay_payment_id,
             "paymentInfo.razorpay_signature": razorpay_signature,
-            isConfirmed: true, 
+            orderStatus: "completed",
           },
         },
         { new: true }
       );
 
-     
       if (updatedOrder) {
-        res.redirect(
-          `https://atomcreations.co/paymentsuccess?reference=${razorpay_payment_id}`
-        );
+        // Check if payment was successful
+        if (updatedOrder.paymentStatus === "success") {
+          res.redirect(
+            `https://atomcreations.co/paymentsuccess?reference=${razorpay_payment_id}`
+          );
+        } else {
+          res.redirect(
+            `https://atomcreations.co/paymentfailed?reference=${razorpay_payment_id}`
+          );
+        }
       } else {
-
         res.status(404).json({ success: false, message: "Order not found" });
       }
     } catch (error) {
-     
       console.error(error);
       res
         .status(500)
         .json({ success: false, message: "Internal server error" });
     }
   } else {
-    
     res.status(400).json({ success: false, message: "Invalid signature" });
   }
 });
